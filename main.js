c1793a76a38550f069fb1cab4dc6518316a1df3c
lsky-walt <lsky_walt@163.com>
2021-10-20 16:30:06 +0800

    fix: online template

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
 
-const commander = require('commander')
-const chalk = require('chalk')
-const generate = require('./lib/index')
-
-
+const commander = require("commander")
+const chalk = require("chalk")
+const generate = require("./lib/index")
 
 // package.json
-const packageJson = require('./package.json')
+const packageJson = require("./package.json")
 
 // global projectName
 let projectName = null
@@ -15,34 +13,28 @@ let projectName = null
 const init = async () => {
   const app = new commander.Command(packageJson.name)
     .version(packageJson.version)
-    .arguments('<project-name>')
-    .usage(`${chalk.green('project-name')} [options]`)
-    .action(name => {
+    .arguments("<project-name>")
+    .usage(`${chalk.green("project-name")} [options]`)
+    .action((name) => {
       projectName = name
     })
-    .option('--use-npm', 'use npm to install')
-    .option('--template <template>', 'use target template', 'react')
-    .option('--online <url>', 'github http url.')
+    .option("--use-npm", "use npm to install")
+    .option("--template <template>", "use target template", "react")
+    .option("--online <url>", "github http url.")
     .allowUnknownOption()
-    .on('--help', () => {
-      console.log('create money tools.')
+    .on("--help", () => {
+      console.log("create money tools.")
     })
     .parse(process.argv)
 
-
-  if(!projectName) {
-    console.error('Project name is required.')
+  if (!projectName) {
+    console.error("Project name is required.")
     return
   }
 
   const opts = app.opts()
-  
-  await generate(
-    projectName,
-    opts.useNpm,
-    opts.online,
-    opts.template,
-  )
+
+  await generate(projectName, opts.useNpm, opts.online, opts.template)
 }
 
 init()
